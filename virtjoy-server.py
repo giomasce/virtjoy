abcfe5b91401d1dd2dea3b72fb708175ab15c8db
Giovanni Mascellani <gio@debian.org>
2017-03-08 15:09:00 +0100

    Implement remote protocol.

--- virtjoy-server.py
+++ virtjoy-server.py
@@ -6,6 +6,7 @@ import uinput
 import time
 import select
 import socket
+import struct
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -13,32 +14,56 @@ from gi.repository import Gtk, Gdk, GLib
 
 BUFSIZE = 2500
 
-BTN_MAP = {
-    #Gdk.KEY_Right: uinput.BTN_DPAD_RIGHT,
-    #Gdk.KEY_Up: uinput.BTN_DPAD_UP,
-    #Gdk.KEY_Left: uinput.BTN_DPAD_LEFT,
-    #Gdk.KEY_Down: uinput.BTN_DPAD_DOWN,
+VERSION = 0
 
-    Gdk.KEY_d: uinput.BTN_C,
-    Gdk.KEY_w: uinput.BTN_Y,
-    Gdk.KEY_a: uinput.BTN_A,
-    Gdk.KEY_s: uinput.BTN_B,
+def split_packet(packet, i):
+    if len(packet) < i:
+        raise Exception()
+    return packet[:i], packet[i:]
 
-    Gdk.KEY_3: uinput.BTN_SELECT,
-    Gdk.KEY_1: uinput.BTN_START,
-    Gdk.KEY_q: uinput.BTN_X,
-    Gdk.KEY_e: uinput.BTN_Z,
+def decode_packet(packet):
+    try:
+        piece, packet = split_packet(packet, 1)
+        version, = struct.unpack('B', piece)
+        if version != VERSION:
+            return None
 
-    Gdk.KEY_c: uinput.BTN_DPAD_UP,
-    Gdk.KEY_v: uinput.BTN_DPAD_DOWN,
-}
+        piece, packet = split_packet(packet, 1)
+        length, = struct.unpack('B', piece)
+        if length == 0:
+            return None
+        piece, packet = split_packet(packet, length)
+        name = piece.decode('utf-8')
 
-AXIS_MAP = {
-    Gdk.KEY_Right: (uinput.ABS_X, 1),
-    Gdk.KEY_Up: (uinput.ABS_Y, -1),
-    Gdk.KEY_Left: (uinput.ABS_X, -1),
-    Gdk.KEY_Down: (uinput.ABS_Y, 1),
-}
+        piece, packet = split_packet(packet, 1)
+        length, = struct.unpack('B', piece)
+        status = {}
+        for i in range(length):
+            piece, packet = split_packet(packet, 5)
+            evtype, elem, value = struct.unpack("!BHh", piece)
+            status[(evtype, elem)] = value
+
+        return name, status
+
+    # Packet is not valid
+    except:
+        return None
+
+def process_packet(packet, devices):
+    name, status = decode_packet(packet)
+    if name not in devices:
+        events = []
+        for elem in status:
+            # Append limits to axes
+            if elem[0] == 3:
+                events.append(elem + (-1, 1, 0, 0))
+            else:
+                events.append(elem)
+        devices[name] = uinput.Device(events, 'virtjoy-{}'.format(name))
+
+    device = devices[name]
+    for elem, value in status.items():
+        device.emit(elem, value)
 
 # uinput code is ased on http://unix.stackexchange.com/a/290606/31311
 # See also http://thiemonge.org/getting-started-with-uinput
@@ -53,24 +78,10 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
 
-    events = []
-    for btn in BTN_MAP.values():
-        events.append(btn)
-    for axis, direction in AXIS_MAP.values():
-        events.append(axis + (-1, 1, 0, 0))
-    device = uinput.Device(events, 'virtjoy')
-
-    #for event in events:
-    #    device.emit(event, 0, syn=False)
-    #while True:
-    #    device.emit(uinput.BTN_NORTH, 1)
-    #    time.sleep(1.0)
-    #    device.emit(uinput.BTN_NORTH, 0)
-    #    time.sleep(1.0)
-
+    devices = {}
     while True:
-        data, addr = sock.recvfrom(BUFSIZE)
-        print("received from {}: {}".format(data, addr))
+        packet, addr = sock.recvfrom(BUFSIZE)
+        process_packet(packet, devices)
 
 if __name__ == '__main__':
     main()
